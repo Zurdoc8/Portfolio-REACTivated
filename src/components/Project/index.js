09a02a16db8843b678b83c5a12f9f2530311076a
Andres <garelan@gmail.com>
2022-06-24 22:08:50 -0500

    more components added

---
+++ src/components/Project/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function ProjectCards(props) {
+    return (
+        <div className='card'>
+            <div className='img-container'>
+                <img alt={props.name} src={props.image}/>
+            </div>
+            <div className='content'>
+                <p className='card-title'>{props.name}</p>
+                <p className='proj-icons-container'>
+                    <a href={props.github}><img class="project-icon" src="https://img.icons8.com/fluent/48/000000/github.png" alt='GitHub Repo' /></a>
+                    <a href={props.deploy}><img class="projet-icon" src='https://img.icons8.com/fluent/48/0000000/domain.png' alt='Live Application' /></a>
+                </p>
+                <p class="topics">
+                    ({props.topics})
+                </p>
+            </div>
+        </div>
+    );
+}
+
+export default ProjectCards;
