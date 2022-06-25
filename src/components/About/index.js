56e4d4281880977075035430863080b67d953a04
Andres <garelan@gmail.com>
2022-06-25 00:12:13 -0500

    compiled successfully but blank page is rendered

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import coverImage from '../../assets/cover/cover-image2.jpg';
+// import coverImage from '../../assets/cover/';
+
 function About() {
   return (
-    <section className="my-5">
-      <h1 id="about">Who am I?</h1>
-      <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover" />
-      <div className="my-2">
+    <section className="container">
+            <h2 class = "top-title" > Welcome! My Name is Andres Garza and I am a Full Stack Web Developer </h2>
+            <hr></hr>
+            <div>
+                < img class = "mb-5"
+                id = "about"
+                src = "https://media.istockphoto.com/photos/programming-code-abstract-technology-background-of-software-developer-picture-id1224500457?k=20&m=1224500457&s=612x612&w=0&h=LpjdY6okVWiKh8LT_LqcXz53kWaXVf47ThXKwMcCoHI="
+                alt = "Let me introduce myself" />
         <p>
           Inmensly proud to call myself a FULL STACK WEB DEVEOLPER. This journey began 5 months ago and I am amazed at how far I've come. I have 
           learned multiple technologies and languages that make me a well rounded Developer. I consider myself a stronger back-end than I am front-end, but I 
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Route, Redirect, HashRouter } from "react-router-dom";
+import { Route, HashRouter } from "react-router-dom";
 import Nav from "../Nav";
 import About from "../About";
 import Portfolio from "../Portfolio";
@@ -15,7 +15,7 @@ class Header extends Component {
                 </header>
 
                 <div className="content">
-                    <Route exact path="/" render={() => <Redirect to="/about" />} />
+                    {/* <Route exact path="/" render={() => <Redirect to="/about" />} /> */}
                     <Route path="/about" component={About} />
                     <Route path="/portfolio" component={Portfolio} />
                     <Route path="/contact" component={Contact} />
