90f6dd953d1cd8a29d7991e24949a23257448336
Andres <garelan@gmail.com>
2022-06-24 21:47:19 -0500

    added new components

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import coverImage from '../../assets/cover/cover-image.jpg';
+import coverImage from '../../assets/cover/cover-image2.jpg';
 function About() {
   return (
     <section className="my-5">
@@ -7,13 +7,19 @@ function About() {
       <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover" />
       <div className="my-2">
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec bibendum turpis sed ex
-          condimentum molestie. Mauris condimentum lectus ut ornare dignissim. Mauris faucibus urna
-          mi, ac feugiat metus aliquam maximus. Proin aliquam justo nec diam vulputate vestibulum.
-          Aenean sollicitudin nulla at nisi ornare, nec suscipit massa eleifend. Morbi tristique
-          justo vel turpis sollicitudin, et tristique velit convallis. In hac habitasse platea
-          dictumst. Phasellus mattis nunc sed orci consequat laoreet. Praesent id nisl nibh.
-          Curabitur imperdiet ultricies mollis. In hac habitasse platea dictumst.
+          Inmensly proud to call myself a FULL STACK WEB DEVEOLPER. This journey began 5 months ago and I am amazed at how far I've come. I have 
+          learned multiple technologies and languages that make me a well rounded Developer. I consider myself a stronger back-end than I am front-end, but I 
+          keep working to hone my skills. So far the MERN Stack is my favorite way to develop web-applications. 
+        </p>
+        <p>
+          This is mereley the beginning of my journey and I hope that during it, I am able to impact the lives of others in a positive and meaningful way.
+          I like to use a creative approach when I deal with problems. I am a self-driven individual ready to take on any challenge, as I continue to grow 
+          in this role. I am gifted in both written and verbal communications and great have interpersonal skills. In my professional experience I have been more than 
+          capable in managing teams as an effective leader.   
+        </p>
+        <p>
+          Welcome to my Portfolio. If you have any questions feel free to reach out. I hope I get the chance to improve the world through a meaningful contribution in the 
+          tech industry.  
         </p>
       </div>
     </section>
---
+++ src/components/Header/index.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import { Route, Redirect, HashRouter } from "react-router-dom";
+import Nav from "../Nav";
+import About from "../About";
+import Portfolio from "../";
+import Contact from "../Contact";
+
+class Header extends Component {
+    render() {
+        return (
+            <HashRouter>
+                <header>
+                    <Nav />
+                </header>
+
+                <div className="content">
+                    <Route exact path="/" render={() => <Redirect to="/about" />} />
+                    <Route path="/about" component={About} />
+                    <Route path="/portfolio" component={Portfolio} />
+                    <Route path="/contact" component={Contact} />
+                </div>
+            </HashRouter>
+        );
+    }
+}
+
+export default Header;
