a3d16ffd261f911a4b7052e76e21876e2763a1f3
Andres <garelan@gmail.com>
2022-11-07 23:45:12 -0600

    fixed NavBar issue

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,39 +1,38 @@
 import React from "react";
-import { Navbar } from "react-bootstrap";
 import {NavLink} from "react-router-dom";
 
 function Nav(props) {
   return (
     <div>
-      <Navbar bg="dark" sticky="top">
+      <div bg="dark" sticky="top" className="nv">
         <NavLink className="nav-link" to="/">
-          <div class="text-green">
-            <h4 class="nav-title-font">Andres Garza</h4>
+          <div className="text-green">
+            <h4 className="nav-title-font">Andres Garza</h4>
           </div>
         </NavLink>
-        <ul class="navitem-indent">
-          <li class="nav-item">
+        <ul className="navitem-indent">
+          <li className="nav-item">
             <NavLink to="/about">
-              <div class="nav-font text-light">About Me</div>
+              <div className="nav-font text-light">About Me</div>
             </NavLink>
           </li>
-          <li class="nav-item">
+          <li className="nav-item">
             <NavLink to="/portfolio">
-              <div class="nav-font text-light">Portfolio</div>
+              <div className="nav-font text-light">Portfolio</div>
             </NavLink>
           </li>
-          <li class="nav-item">
+          <li className="nav-item">
             <NavLink to="/contact">
-              <div class="nav-font text-light">Contact</div>
+              <div className="nav-font text-light">Contact</div>
             </NavLink>
           </li>
-          <li class="nav-item">
+          <li className="nav-item">
             <NavLink to="/resume">
-              <div class="nav-font text-light">Resume</div>
+              <div className="nav-font text-light">Resume</div>
             </NavLink>
           </li>
         </ul>
-      </Navbar>
+      </div>
     </div>
   );
 }
